0ee7071a7073181772151f3fd24eecd8a9fd7c6b
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-10-17 16:54:15 +0600

    [fix]: corrupt xlsx fix by wrapping sheet with worksheet

--- src/xl/worksheets/sheet1.xml.ts
+++ src/xl/worksheets/sheet1.xml.ts
@@ -26,9 +26,29 @@ const cell = (row: string, value: ICellEntry) =>
 
 const cellValue = (value: ICellEntry) => `<v>${value.value}</v>`;
 
-const generateSheet1Xml = (grid: ICellEntry[][]) =>
+const generateSheetData = (grid: ICellEntry[][]) =>
   `<sheetData>${grid
     .map((ro: ICellEntry[], index: number) => row(index, ro))
     .join("")}</sheetData>`;
+
+
+
+
+const generateSheet1Xml = (
+    grid: ICellEntry[][]
+) => `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac xr xr2 xr3" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:xr="http://schemas.microsoft.com/office/spreadsheetml/2014/revision" xmlns:xr2="http://schemas.microsoft.com/office/spreadsheetml/2015/revision2" xmlns:xr3="http://schemas.microsoft.com/office/spreadsheetml/2016/revision3" xr:uid="{6A0CD924-A203-41BB-8331-42E46B7D5F20}">
+	<dimension ref="B3:I7" />
+	<sheetViews>
+		<sheetView tabSelected="1" workbookViewId="0">
+			<selection activeCell="B3" sqref="B3:D4" />
+		</sheetView>
+	</sheetViews>
+	<sheetFormatPr defaultRowHeight="15" x14ac:dyDescent="0.25" />
+    ${generateSheetData(grid)}
+    <pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" footer="0.3" />
+</worksheet>
+`;
+
 
 export { ICellEntry, generateSheet1Xml };
