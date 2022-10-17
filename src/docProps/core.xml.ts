b5a99f6ee590b93bf7723bdcedddfbd259ac7868
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-10-17 11:23:03 +0600

    scafollding based on excel: 1

---
+++ src/docProps/core.xml.ts
@@ -0,0 +1,17 @@
+const generateCoreXml = () =>
+  `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <creator>
+        Hp
+    </creator>
+    <lastModifiedBy>
+        Hp
+    </lastModifiedBy>
+    <created xsi:type="dcterms:W3CDTF">
+        2022-08-11T16:01:16Z
+    </created>
+    <modified xsi:type="dcterms:W3CDTF">
+        2022-08-11T16:18:46Z
+    </modified>
+</coreProperties>
+`;
