d00d9e15f56fd5bf79b512599165316c665281df
maifeeulasad <maifeeulasad@gmail.com>
2023-09-22 19:26:51 +0600

    more detailed core generator w more details;

--- src/docProps/core.xml.ts
+++ src/docProps/core.xml.ts
@@ -1,26 +1,41 @@
 import { format } from "date-fns";
 
-const generateCoreXml = (username: string = "to-spreadsheet") => {
-  const date =
-    format(Date.now(), "yyyy-MM-dd") +
-    "T" +
-    format(Date.now(), "HH:mm:ss") +
-    "Z";
-  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <creator>
+interface IGenerateCoreXml {
+    username?: string
+    description?: string
+    language?: string
+    version?: string
+    subject?: string
+    title?: string
+}
+
+const generateCoreXml = ({ username = "to-spreadsheet", description = "", language = "en-US", version = "1", subject = "", title = "" }: IGenerateCoreXml) => {
+    const date =
+        format(Date.now(), "yyyy-MM-dd") +
+        "T" +
+        format(Date.now(), "HH:mm:ss") +
+        "Z";
+
+    return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+  <cp:coreProperties
+          xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" 
+          xmlns:dc="http://purl.org/dc/elements/1.1/" 
+          xmlns:dcterms="http://purl.org/dc/terms/" 
+          xmlns:dcmitype="http://purl.org/dc/dcmitype/" 
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <dcterms:created xsi:type="dcterms:W3CDTF">${date}</dcterms:created>
+      <dc:creator>
         ${username}
-    </creator>
-    <lastModifiedBy>
-        ${username}
-    </lastModifiedBy>
-    <created xsi:type="dcterms:W3CDTF">
-        ${date}
-    </created>
-    <modified xsi:type="dcterms:W3CDTF">
-        ${date}
-    </modified>
-</coreProperties>
+    </dc:creator>
+      <dc:description>
+      ${description}</dc:description>
+      <dc:language>${language}</dc:language>
+      <cp:lastModifiedBy>${date}</cp:lastModifiedBy>
+      <dcterms:modified xsi:type="dcterms:W3CDTF">${date}</dcterms:modified>
+      <cp:revision>${version}</cp:revision>
+      <dc:subject>${subject}</dc:subject>
+      <dc:title>${title}</dc:title>
+  </cp:coreProperties>
 `;
 };
 
