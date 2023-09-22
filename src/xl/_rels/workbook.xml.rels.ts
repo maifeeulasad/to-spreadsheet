9ac2bcdd5e20361aa8c950257a699540f9862c4e
maifeeulasad <maifeeulasad@gmail.com>
2023-09-22 19:25:24 +0600

    workbook relation;

--- src/xl/_rels/workbook.xml.rels.ts
+++ src/xl/_rels/workbook.xml.rels.ts
@@ -1,10 +1,15 @@
-const generateWorkBookXmlRels = () =>
-  `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-    <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml" />
-    <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme" Target="theme/theme1.xml" />
-    <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml" />
-</Relationships>
+import { IWorkbook } from "../..";
+import { indexToVbIndex, indexToVbRelationIndex } from '../../util'
+
+const generateWorkBookXmlRels = (workbook: IWorkbook) =>
+	`<?xml version="1.0" encoding="UTF-8"?>
+	<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+		<Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/>
+		<Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/>
+		${workbook.sheets.map((_, index) => `
+		<Relationship Id="rId${indexToVbRelationIndex(index)}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet${indexToVbIndex(index)}.xml"/>
+		`).join('\n')}
+	</Relationships>
 `;
 
 export { generateWorkBookXmlRels };
