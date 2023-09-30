dc356189d72f9665807748fb30f44154d7dcffec
maifeeulasad <maifeeulasad@gmail.com>
2023-09-30 22:30:10 +0600

    [feature]: modify data type to incorporate function; #8

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { generateExcel, EnvironmentType } from "./generate-excel";
-import { SkipCell, skipCell } from "./util";
+import { SkipCell, skipCell, Equation, writeEquation } from "./util";
 
 enum ICellType {
   string = "s",
   number = "n",
   skip = "skip",
+  equation = "equation",
 }
 
-interface ICell {
-  type: ICellType;
-  value: number | undefined;
+interface ICellString {
+  type: ICellType.string;
+  value: number;
 }
+interface ICellNumber {
+  type: ICellType.number;
+  value: number;
+}
+interface ICellSkip {
+  type: ICellType.skip;
+  value: undefined;
+}
+interface ICellEquation {
+  type: ICellType.equation;
+  value: Equation;
+}
+
+type ICell = ICellString | ICellNumber | ICellSkip | ICellEquation;
 
 interface IRows {
   cells: ICell[];
@@ -29,7 +44,7 @@ interface IWorkbook {
 
 interface IPage {
   title: string
-  content: (string | number | undefined | SkipCell)[][]
+  content: (string | number | undefined | SkipCell | Equation)[][]
 }
 
 export { ICell, ISheet, IWorkbook, IRows, ICellType, IPage }
@@ -42,7 +57,7 @@ const sampleData = [
       ['woof', 'smack'],
       [1],
       [1, 2],
-      [1, 2, 3],
+      [1, 2, 3, writeEquation('SUM(A5,C5)')],
     ]
   },
   { title: 'Maifee2', content: [[1], [1, skipCell(3), 2]] },
@@ -51,4 +66,4 @@ const sampleData = [
 
 // generateExcel(sampleData) // <- check before releasing with `yarn test:compile`
 
-export { generateExcel, sampleData, EnvironmentType, skipCell };
+export { generateExcel, sampleData, EnvironmentType, skipCell, writeEquation };
